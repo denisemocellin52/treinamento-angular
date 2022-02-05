c5a3929ea1aae056aa71932e0364d31bf371c994
Denise Mocellin <drmocellin52@hotmail.com>
2022-02-04 22:20:31 -0300

    Criando um novo componente

---
+++ bankline/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NovaTransferenciaComponent } from './nova-transferência/nova-transferencia.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTransferenciaComponent
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
